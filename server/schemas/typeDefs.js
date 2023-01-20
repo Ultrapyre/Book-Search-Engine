6abc03ea55d17a2afdcdcbe4cfec6d0448af9434
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2023-01-21 04:34:01 +0800

    Added typedefs and resolvers... kind of. Resolver paths are required.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express')
+
+const TypeDefs = gql`
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login:
+        addUser:
+        saveBook:
+        removeBook:
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token:
+        user: User
+    }
+`
+
+module.exports = typeDefs;
